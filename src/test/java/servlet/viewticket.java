c42b88e5214edfa426ff1951a93ba2f9f66bc488
RoshanChowdhury <11698928+RoshanChowdhury@users.noreply.github.com>
2021-03-04 14:56:39 -0500

    Update viewticket.java

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://52.14.189.198:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://3.22.216.136:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
